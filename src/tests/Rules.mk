5e3ace6fea84bf65e7d3fdc15b19bbdb36e4ad0a
JBailes <jbailes@gmail.com>
2026-07-17 09:13:03 +0000

    test(build): link provider_slots_reap.o into unit-test-server-dispatch
    That target links server_provider_slots.o (which now calls
    provider_slots_reap_stamps) but not the new pure TU, so CI's unit-tests failed
    with an undefined reference. Add the object to its link line.

--- src/tests/Rules.mk
+++ src/tests/Rules.mk
@@ -1298,7 +1298,7 @@ $(TESTPREFIX)/unit-test-acp-server: $(OBJDIR)/tests/test_acp_server.o \
                            $(OBJDIR)/cJSON.o
 	$(TESTLINK_MIN) -o $@ $^ $(L_MINIMAL)
 
-$(TESTPREFIX)/unit-test-server-dispatch: $(OBJDIR)/tests/test_server_dispatch.o $(OBJDIR)/server/server.o $(OBJDIR)/server/server_seed_config.o $(OBJDIR)/server/server_api_status.o $(OBJDIR)/server_provider.o $(OBJDIR)/server_insights.o $(OBJDIR)/server_eval.o $(OBJDIR)/server_provider_slots.o \
+$(TESTPREFIX)/unit-test-server-dispatch: $(OBJDIR)/tests/test_server_dispatch.o $(OBJDIR)/server/server.o $(OBJDIR)/server/server_seed_config.o $(OBJDIR)/server/server_api_status.o $(OBJDIR)/server_provider.o $(OBJDIR)/server_insights.o $(OBJDIR)/server_eval.o $(OBJDIR)/server_provider_slots.o $(OBJDIR)/provider_slots_reap.o \
 	$(OBJDIR)/server/s2_native_gate_hook.o $(OBJDIR)/workflow/wfe_native_gate.o $(OBJDIR)/workflow/wfe_externalization.o \
 	$(OBJDIR)/db1/wfe_binding.o $(OBJDIR)/db1/wfe_store.o $(OBJDIR)/workflow/wfe_enforce.o \
                       $(OBJDIR)/harness_memory_common.o $(OBJDIR)/server/delegate_sandbox_image.o \
