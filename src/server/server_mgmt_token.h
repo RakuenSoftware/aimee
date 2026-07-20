db94816b5452887a59edb01b17b8369f3fc8752f
JBailes <jbailes@gmail.com>
2026-07-20 12:29:56 +0000

    feat(p5): bind management JWT claims to peer cert

--- src/server/server_mgmt_token.h
+++ src/server/server_mgmt_token.h
@@ -3,4 +3,8 @@
 #include "kb_verifier.h"
 int server_mgmt_token_verify(const char *jwt, const char *jwks_json, const char *issuer,
                              const char *audience, long now, kb_verify_result_t *out);
+int server_mgmt_token_verify_bound(const char *jwt, const char *jwks_json, const char *issuer,
+                                   const char *audience, const char *peer_cert_cn, long now,
+                                   kb_verify_result_t *out, char *capability, size_t cap_n,
+                                   char *jti, size_t jti_n);
 #endif
