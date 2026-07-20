695afdd92975e327f6e81031364bc7e6ca1dc52b
JBailes <jbailes@gmail.com>
2026-07-20 14:27:31 +0000

    fix(p7): gate KMS live keys on signed HWM

--- src/modules/vault/vault_custody_kms.h
+++ src/modules/vault/vault_custody_kms.h
@@ -2,4 +2,6 @@
 #define AIMEE_VAULT_CUSTODY_KMS_H
 #include "vault_internal.h"
 const vault_custody_provider_t *vault_custody_kms_provider(void);
+int vault_custody_kms_hwm_refresh(void);
+int vault_custody_kms_hwm_ready(void);
 #endif
